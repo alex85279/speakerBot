5d41f588d2a28b42510c29a83ac8ce20228d57f1
oscar <oscarada87@gmail.com>
2020-08-23 14:15:28 +0800

    fix env and routes

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@ import discord
 from discord.ext import commands
 import json
 import os
+from dotenv import load_dotenv
 
-with open('setting.json', 'r', encoding='utf8') as setting:
-    jdata = json.load(setting)
+load_dotenv()
 
 bot = commands.Bot(command_prefix='!')
 
@@ -15,13 +15,13 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(F'{member}join!')
-    channel = bot.get_channel(int(jdata["RoomId"]))
+    channel = bot.get_channel(int(os.getenv('ROOMID')))
     await channel.send(F'{member} 真沒想到你能來到這裡，現在你可以滾了')
 
 @bot.event
 async def on_member_remove(member):
     print(F'{member}leave!')
-    channel = bot.get_channel(int(jdata["RoomId"]))
+    channel = bot.get_channel(int(os.getenv('ROOMID')))
     await channel.send('我的天，他真的滾了')
 
 for filename in os.listdir('./cmds'):
@@ -30,4 +30,4 @@ for filename in os.listdir('./cmds'):
 
 
 if __name__ == "__main__":
-    bot.run(jdata['TOKEN'])
+    bot.run(os.getenv('TOKEN'))
